ac49a21412151dc89d1e9fe783da6d9c10edb895
Ben Foster <ben.foster@checkout.com>
2017-10-26 22:14:41 +0100

    Updated C# casing

--- spec/code_samples/C#/charges/post.cs
+++ spec/code_samples/C#/charges/post.cs
@@ -1,8 +1,8 @@
 
 var client = new GatewayApiClient(apiKey);
 var response = await client.Charges.RequestAsync(new ChargeRequest {
-    amount = 6450,
-    currency = "USD",
-    source = "src_3cdac40561494c5aaa8f8c81ef245c10",
-    capture = false
+    Amount = 6450,
+    Currency = "USD",
+    Source = "src_3cdac40561494c5aaa8f8c81ef245c10",
+    Capture = false
 });
