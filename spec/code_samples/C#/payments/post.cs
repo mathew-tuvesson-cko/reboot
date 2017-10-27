ebd45d50f22c6ecca266239afeade73e12ec2691
Ben Foster <ben.foster@checkout.com>
2017-10-27 11:12:57 +0100

    Charges > Payments

--- spec/code_samples/C#/payments/post.cs
+++ spec/code_samples/C#/payments/post.cs
@@ -1,6 +1,5 @@
-
 var client = new GatewayApiClient(apiKey);
-var response = await client.Charges.RequestAsync(new ChargeRequest {
+var response = await client.Payments.RequestAsync(new PaymentRequest {
     Amount = 6450,
     Currency = "USD",
     Source = "src_3cdac40561494c5aaa8f8c81ef245c10",
